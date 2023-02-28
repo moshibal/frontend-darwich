8c2e9c1805f0ec66317dbefe010fcca2cd6e477c
Bishal karki <bishalcarki2020@gmail.com>
2023-02-28 19:38:49 +1100

    deployed to s3

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -1,6 +1,14 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+// const github = require("@actions/github");
+const exec = require("@actions/exec");
 function run() {
+  //Get some input values
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+  //Upload Files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
   core.notice("hello from my custom javascript action");
 }
 run();
