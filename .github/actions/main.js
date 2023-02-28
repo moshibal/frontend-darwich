80977dabd2cdd342a2c277bde80dcb63183c93e0
Bishal karki <bishalcarki2020@gmail.com>
2023-02-28 18:11:37 +1100

    changed in action.yml

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -1,2 +1,6 @@
-function run() {}
+const core = require("@actions/core");
+const github = require("@actions/github");
+function run() {
+  core.notice("hello from my custom javascript action");
+}
 run();
