0cdb72be6fc4132efa509e3bb474abdc602acede
Bishal karki <bishalcarki2020@gmail.com>
2023-03-02 11:57:14 +1100

    fixed issue

--- src/components/Form/SignUp.js
+++ src/components/Form/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import styles from "./SignUp.module.css";
 import Message from "../Utility/Message";
 import { register } from "../../store/register-slice";
