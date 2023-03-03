6e7f670a951895227483facc8733811641143acb
Bishal karki <bishalcarki2020@gmail.com>
2023-03-04 09:45:56 +1100

    changes made in github actions

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -8,7 +8,9 @@ function run() {
   const distFolder = core.getInput("dist-folder", { required: true });
   //Upload Files
   const s3Uri = `s3://${bucket}`;
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+  exec.exec(
+    `aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion} --delete`
+  );
   core.notice("hello from my custom javascript action");
 }
 run();
